3aeac68f99bf87a60a7728b5c835e125730e379b
BuildTools <lopezk38@gmail.com>
2018-12-26 04:05:03 -0800

    First commit

---
+++ csgoWalk/feeder.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using vJoyInterfaceWrap;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace csgoWalk
+{
+    class Feeder
+    {
+
+        static public vJoy joystick;
+        static public vJoy.JoystickState iReport;
+        static public uint id;
+
+        static private readonly uint centerVal = 16383;
+        static private readonly uint offsetVal = 5926;
+        static private readonly uint[] lookUp = new uint[] {};
+
+        private uint X = centerVal;
+        private uint Y = centerVal;
+
+        private bool leftKeyDown = false;
+        private bool rightKeyDown = false;
+        private bool forwardKeyDown = false;
+        private bool backwardKeyDown = false;
+
+        private Keys leftKey = Keys.Left;
+        private Keys rightKey = Keys.Right;
+        private Keys forwardKey = Keys.Up;
+        private Keys backwardKey = Keys.Down;
+
+
+        private static void KbHook_KeyDown(Keys key, bool Shift, bool Ctrl, bool Alt)
+        {
+            
+        }
+
+        private static void KbHook_KeyUp(Keys key, bool Shift, bool Ctrl, bool Alt)
+        {
+            
+        }
+
+        public void SetKeyBinds(string direction, Keys key)
+        {
+            switch (direction)
+            {
+                case "left":
+                    leftKeyDown = false;
+                    leftKey = key;
+                    break;
+                case "right":
+                    rightKeyDown = false;
+                    rightKey = key;
+                    break;
+                case "forward":
+                    forwardKeyDown = false;
+                    forwardKey = key;
+                    break;
+                case "backward":
+                    backwardKeyDown = false;
+                    backwardKey = key;
+                    break;
+                default:
+                    break;
+            }
+            return;
+        }
+
+        public Feeder() : this(1) //calls uint constructor with args of 1
+        {
+
+        }
+
+        public Feeder(uint inputId) //From the vJoy SDK with adjustment from lopezk38
+        {
+            // Create one joystick object and a position structure.
+            joystick = new vJoy();
+            iReport = new vJoy.JoystickState();
+
+            // Device ID can only be in the range 1-16
+            if (id <= 0 || id > 16)
+            {
+                Console.WriteLine("Illegal device ID {0}\nExit!", id);
+                return;
+            }
+
+            // Get the driver attributes (Vendor ID, Product ID, Version Number)
+            if (!joystick.vJoyEnabled())
+            {
+                Console.WriteLine("vJoy driver not enabled: Failed Getting vJoy attributes.\n");
+                return;
+            }
+            else
+                Console.WriteLine("Vendor: {0}\nProduct :{1}\nVersion Number:{2}\n", joystick.GetvJoyManufacturerString(), joystick.GetvJoyProductString(), joystick.GetvJoySerialNumberString());
+
+            // Get the state of the requested device
+            VjdStat status = joystick.GetVJDStatus(id);
+            switch (status)
+            {
+                case VjdStat.VJD_STAT_OWN:
+                    Console.WriteLine("vJoy Device {0} is already owned by this feeder\n", id);
+                    break;
+                case VjdStat.VJD_STAT_FREE:
+                    Console.WriteLine("vJoy Device {0} is free\n", id);
+                    break;
+                case VjdStat.VJD_STAT_BUSY:
+                    Console.WriteLine("vJoy Device {0} is already owned by another feeder\nCannot continue\n", id);
+                    return;
+                case VjdStat.VJD_STAT_MISS:
+                    Console.WriteLine("vJoy Device {0} is not installed or disabled\nCannot continue\n", id);
+                    return;
+                default:
+                    Console.WriteLine("vJoy Device {0} general error\nCannot continue\n", id);
+                    return;
+            };
+
+            // Check which axes are supported
+            bool AxisX = joystick.GetVJDAxisExist(id, HID_USAGES.HID_USAGE_X);
+            bool AxisY = joystick.GetVJDAxisExist(id, HID_USAGES.HID_USAGE_Y);
+
+            // Print results
+            Console.WriteLine("\nvJoy Device {0} capabilities:\n", id);
+            Console.WriteLine("Axis X\t\t{0}\n", AxisX ? "Yes" : "No");
+            Console.WriteLine("Axis Y\t\t{0}\n", AxisX ? "Yes" : "No");
+
+            // Test if DLL matches the driver
+            UInt32 DllVer = 0, DrvVer = 0;
+            bool match = joystick.DriverMatch(ref DllVer, ref DrvVer);
+            if (match)
+                Console.WriteLine("Version of Driver Matches DLL Version ({0:X})\n", DllVer);
+            else
+                Console.WriteLine("Version of Driver ({0:X}) does NOT match DLL Version ({1:X})\n", DrvVer, DllVer);
+
+
+            // Acquire the target
+            if ((status == VjdStat.VJD_STAT_OWN) || ((status == VjdStat.VJD_STAT_FREE) && (!joystick.AcquireVJD(id))))
+            {
+                Console.WriteLine("Failed to acquire vJoy device number {0}.\n", id);
+                return;
+            }
+            else
+                Console.WriteLine("Acquired: vJoy device number {0}.\n", id);
+
+            long maxVal = 0;
+
+            joystick.GetVJDAxisMax(id, HID_USAGES.HID_USAGE_X, ref maxVal);
+
+            var kbHook = new KeyboardHook(true);
+            kbHook.KeyDown += KbHook_KeyDown;
+            kbHook.KeyUp += KbHook_KeyDown;
+
+            while (true)
+            {
+                iReport.bDevice = (byte)id;
+                iReport.AxisX = X;
+                iReport.AxisY = Y;
+
+                /*** Feed the driver with the position packet - is fails then wait for input then try to re-acquire device ***/
+                if (!joystick.UpdateVJD(id, ref iReport))
+                {
+                    Console.WriteLine("Feeding vJoy device number {0} failed - try to enable device then press enter\n", id);
+                    Console.ReadKey(true);
+                    joystick.AcquireVJD(id);
+                }
+
+                System.Threading.Thread.Sleep(20);
+
+                Y = (int)mapper1.Map(tempY);
+                X = (int)halfVal + ((tempX1 / 2) - (tempX2 / 2));
+            }; // While
+        }
+    }
+
+    public class KeyboardHook : IDisposable //By Wolf5 at https://stackoverflow.com/questions/34281223/c-sharp-hook-global-keyboard-events-net-4-0
+    {
+        bool Global = false;
+
+        public delegate void LocalKeyEventHandler(Keys key, bool Shift, bool Ctrl, bool Alt);
+        public event LocalKeyEventHandler KeyDown;
+        public event LocalKeyEventHandler KeyUp;
+
+        public delegate int CallbackDelegate(int Code, int W, int L);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        public struct KBDLLHookStruct
+        {
+            public Int32 vkCode;
+            public Int32 scanCode;
+            public Int32 flags;
+            public Int32 time;
+            public Int32 dwExtraInfo;
+        }
+
+        [DllImport("user32", CallingConvention = CallingConvention.StdCall)]
+        private static extern int SetWindowsHookEx(HookType idHook, CallbackDelegate lpfn, int hInstance, int threadId);
+
+        [DllImport("user32", CallingConvention = CallingConvention.StdCall)]
+        private static extern bool UnhookWindowsHookEx(int idHook);
+
+        [DllImport("user32", CallingConvention = CallingConvention.StdCall)]
+        private static extern int CallNextHookEx(int idHook, int nCode, int wParam, int lParam);
+
+        [DllImport("kernel32.dll", CallingConvention = CallingConvention.StdCall)]
+        private static extern int GetCurrentThreadId();
+
+        public enum HookType : int
+        {
+            WH_JOURNALRECORD = 0,
+            WH_JOURNALPLAYBACK = 1,
+            WH_KEYBOARD = 2,
+            WH_GETMESSAGE = 3,
+            WH_CALLWNDPROC = 4,
+            WH_CBT = 5,
+            WH_SYSMSGFILTER = 6,
+            WH_MOUSE = 7,
+            WH_HARDWARE = 8,
+            WH_DEBUG = 9,
+            WH_SHELL = 10,
+            WH_FOREGROUNDIDLE = 11,
+            WH_CALLWNDPROCRET = 12,
+            WH_KEYBOARD_LL = 13,
+            WH_MOUSE_LL = 14
+        }
+
+        private int HookID = 0;
+        CallbackDelegate TheHookCB = null;
+
+        //Start hook
+        public KeyboardHook(bool Global)
+        {
+            this.Global = Global;
+            TheHookCB = new CallbackDelegate(KeybHookProc);
+            if (Global)
+            {
+                HookID = SetWindowsHookEx(HookType.WH_KEYBOARD_LL, TheHookCB,
+                    0, //0 for local hook. eller hwnd til user32 for global
+                    0); //0 for global hook. eller thread for hooken
+            }
+            else
+            {
+                HookID = SetWindowsHookEx(HookType.WH_KEYBOARD, TheHookCB,
+                    0, //0 for local hook. or hwnd to user32 for global
+                    GetCurrentThreadId()); //0 for global hook. or thread for the hook
+            }
+        }
+
+        bool IsFinalized = false;
+        ~KeyboardHook()
+        {
+            if (!IsFinalized)
+            {
+                UnhookWindowsHookEx(HookID);
+                IsFinalized = true;
+            }
+        }
+        public void Dispose()
+        {
+            if (!IsFinalized)
+            {
+                UnhookWindowsHookEx(HookID);
+                IsFinalized = true;
+            }
+        }
+
+        //The listener that will trigger events
+        private int KeybHookProc(int Code, int W, int L)
+        {
+            KBDLLHookStruct LS = new KBDLLHookStruct();
+            if (Code < 0)
+            {
+                return CallNextHookEx(HookID, Code, W, L);
+            }
+            try
+            {
+                if (!Global)
+                {
+                    if (Code == 3)
+                    {
+                        IntPtr ptr = IntPtr.Zero;
+
+                        int keydownup = L >> 30;
+                        if (keydownup == 0)
+                        {
+                            if (KeyDown != null) KeyDown((Keys)W, GetShiftPressed(), GetCtrlPressed(), GetAltPressed());
+                        }
+                        if (keydownup == -1)
+                        {
+                            if (KeyUp != null) KeyUp((Keys)W, GetShiftPressed(), GetCtrlPressed(), GetAltPressed());
+                        }
+                        //System.Diagnostics.Debug.WriteLine("Down: " + (Keys)W);
+                    }
+                }
+                else
+                {
+                    KeyEvents kEvent = (KeyEvents)W;
+
+                    Int32 vkCode = Marshal.ReadInt32((IntPtr)L); //Leser vkCode som er de første 32 bits hvor L peker.
+
+                    if (kEvent != KeyEvents.KeyDown && kEvent != KeyEvents.KeyUp && kEvent != KeyEvents.SKeyDown && kEvent != KeyEvents.SKeyUp)
+                    {
+                    }
+                    if (kEvent == KeyEvents.KeyDown || kEvent == KeyEvents.SKeyDown)
+                    {
+                        if (KeyDown != null) KeyDown((Keys)vkCode, GetShiftPressed(), GetCtrlPressed(), GetAltPressed());
+                    }
+                    if (kEvent == KeyEvents.KeyUp || kEvent == KeyEvents.SKeyUp)
+                    {
+                        if (KeyUp != null) KeyUp((Keys)vkCode, GetShiftPressed(), GetCtrlPressed(), GetAltPressed());
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                //Ignore all errors...
+            }
+
+            return CallNextHookEx(HookID, Code, W, L);
+
+        }
+
+        public enum KeyEvents
+        {
+            KeyDown = 0x0100,
+            KeyUp = 0x0101,
+            SKeyDown = 0x0104,
+            SKeyUp = 0x0105
+        }
+
+        [DllImport("user32.dll")]
+        static public extern short GetKeyState(System.Windows.Forms.Keys nVirtKey);
+
+        public static bool GetCapslock()
+        {
+            return Convert.ToBoolean(GetKeyState(System.Windows.Forms.Keys.CapsLock)) & true;
+        }
+        public static bool GetNumlock()
+        {
+            return Convert.ToBoolean(GetKeyState(System.Windows.Forms.Keys.NumLock)) & true;
+        }
+        public static bool GetScrollLock()
+        {
+            return Convert.ToBoolean(GetKeyState(System.Windows.Forms.Keys.Scroll)) & true;
+        }
+        public static bool GetShiftPressed()
+        {
+            int state = GetKeyState(System.Windows.Forms.Keys.ShiftKey);
+            if (state > 1 || state < -1) return true;
+            return false;
+        }
+        public static bool GetCtrlPressed()
+        {
+            int state = GetKeyState(System.Windows.Forms.Keys.ControlKey);
+            if (state > 1 || state < -1) return true;
+            return false;
+        }
+        public static bool GetAltPressed()
+        {
+            int state = GetKeyState(System.Windows.Forms.Keys.Menu);
+            if (state > 1 || state < -1) return true;
+            return false;
+        }
+    }
+}
